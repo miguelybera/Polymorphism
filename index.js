2b62ae1befc87b2a09fe54467b7bfe33ed43784f
CISCO <cisco@iicsadmin.com>
2020-03-02 13:59:46 +0800

    TryCatchError

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
-class Person{
-    constructor(name){
-        this.name = name;
-    }
+try{
+    sayHello();
+}catch(err){
+    document.write(err.message +"<br>");
+}finally{
+document.write("inside the finally block")
 }
-
-class Student extends Person{
-    constructor(name, course){
-        super(name);
-        this.course = course
-    }
-}
-
-let p = new Person("Juan Dela Cruz");
-let s = new Student("Pedro Morales", "IT");
-document.write(typeof(s) + "<br>");
-s=1;
-document.write(s);
-document.write(`<br>Hello ${p.name}<br>`);
-p=s;
-document.write(`Hello ${s.name}`);
